df2ece16ab1941c74e47f49d4a13cec0e6b4e350
alexcoder04 <ailyushonak@gmail.com>
2021-12-02 19:10:03 +0100

    implement couple of commands

--- README.md
+++ README.md
@@ -1,5 +1,29 @@
 
 # rfap-pycli
 
-CLI Client for rfap
+CLI Client for rfap. See [related projects](#related-projects) for compatible
+applications and protocol specification.
+
+## Usage
+
+```
+./rfap_pycli.py [-s server-address]
+```
+
+### Commands
+
+| commands             | description              |
+|----------------------|--------------------------|
+| `help`               | print help               |
+| `pwd`                | print working directory  |
+| `cd <folder>`        | change working directory |
+| `ls`, `list`, `dir`  | list current directory   |
+| `exit`, `quit`, `:q` | disconnect and exit      |
+
+## Related projects
+
+ - https://github.com/alexcoder04/rfap - protocol specification
+ - https://github.com/alexcoder04/rfap-go-server - reference server implementation
+ - https://github.com/alexcoder04/librfap - Python library
+ - https://github.com/BoettcherDasOriginal/rfab-cs-lib - C# library
 
--- rfap_pycli.py
+++ rfap_pycli.py
@@ -6,6 +6,33 @@ import sys
 
 PROMPT = "rfap> "
 
+def enter_command():
+    inp = input(PROMPT).split()
+    return inp[0], inp[1:]
+
+def command_cd(oldpwd):
+    if args[0] == "..":
+        if oldpwd == "/":
+            print(oldpwd)
+            return "/"
+        pwd = "/" + "/".join(oldpwd.split("/")[:-1])
+    else:
+        pwd = oldpwd
+        while pwd.endswith("/"):
+            pwd = pwd[:-1]
+        pwd = pwd + "/" + args[0]
+    metadata = client.rfap_info(pwd)
+    if metadata["ErrorCode"] != 0:
+        print(f"cannot cd to '{pwd}': {metadata['ErrorMessage']}")
+        pwd = oldpwd
+    else:
+        if metadata["Type"] != "d":
+            print(f"cannot cd to '{pwd}': not a directory")
+            pwd = oldpwd
+        else:
+            print(pwd)
+    return pwd
+
 if __name__ == "__main__":
     print("Welcome to rfap-pycli!")
     server_address = None
@@ -23,9 +50,26 @@ if __name__ == "__main__":
     print("Connecting to", server_address)
     client = librfap.Client(server_address)
 
-    command = input(PROMPT)
+    pwd = "/"
+
+    command, args = enter_command()
     while command not in ("exit", "quit", ":q"):
-        command = input(PROMPT)
+        if command in ("help"):
+            print("help is coming soon xD")
+        elif command in ("pwd"):
+            print(pwd)
+        elif command in ("cd"):
+            pwd = command_cd(pwd)
+        elif command in ("ls", "list", "dir"):
+            metadata, files = client.rfap_directory_read(pwd)
+            if metadata["ErrorCode"] != 0:
+                print("Error:", metadata["ErrorMessage"])
+            else:
+                for f in files:
+                    print(f)
+        else:
+            print("type 'help' for help")
+        command, args = enter_command()
 
     print("disconnecting...")
     client.rfap_disconnect()
